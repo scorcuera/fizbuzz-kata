9bf5af65a36dd4b2d872959fdd7d78bac1738f82
scorcuera <scorcuera6@gmail.com>
2022-12-20 15:22:41 +0100

    added test suites to test file

--- __tests__/fizzbuzz.test.js
+++ __tests__/fizzbuzz.test.js
@@ -1,1 +1,26 @@
 import { fizzbuzz } from "../src/fizzbuzz";
+
+test("debe devolver 'Fizz' si introduzco 3", () => {
+    let result = fizzbuzz(3);
+    expect(result).toBe('Fizz');
+})
+
+test("debe devolver 'Buzz' si introduzco 5", () => {
+    let result = fizzbuzz(5);
+    expect(result).toBe('Buzz');
+})
+
+test("debe devolver 'FizzBuzz' si introduzco 15", () => {
+    let result = fizzbuzz(15);
+    expect(result).toBe('FizzBuzz');
+})
+
+test("debe devolver 'Fizz' si introduzco 6", () => {
+    let result = fizzbuzz(6);
+    expect(result).toBe('Fizz');
+})
+
+test("debe devolver 'Buzz' si introduzco 20", () => {
+    let result = fizzbuzz(20);
+    expect(result).toBe('Buzz');
+})
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,4 @@
-function fizzbuzz() {
+function fizzbuzz(num) {
 
 }
 
